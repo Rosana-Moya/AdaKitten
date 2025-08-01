05736c1bc3fb6cc3d96e028de5e2b1fcff83a54d
Rosana-Moya <rosanamoya88@gmail.com>
2025-08-01 13:23:09 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict"
+
+console.log("Go");
+
+document.querySelector(".js-list").innerHTML 
+
+/* Agregar el código del li desde HTMl 
+Repetir este proceso por cada gatito */
+const kittenOne = `<li class="card">
+<article>
+  <img
+    class="card_img"
+    src="https://dev.adalab.es/gato-siames.webp"
+    alt="gatito"
+  />
+  <h3 class="card_title">Anastacio</h3>
+  <h4 class="card_race">Siamés</h4>
+  <p class="card_description">
+            Porte elegante, su patrón de color tan característico y sus ojos
+            de un azul intenso, pero su historia se remonta a Asía al menos
+            hace 500 años, donde tuvo su origen muy posiblemente.
+   </p>
+</article>
+</li>`;
+
+console.log(kittenOne);
+
+ 
